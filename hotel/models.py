7a215776a6eccfe4fd49f6addbc3a00c0eebd776
Boset <boss-x86@protonmail.com>
2024-11-19 15:56:31 -0600

    patch

--- hotel/models.py
+++ hotel/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Administrador(models.Model):
     id = models.BigAutoField(primary_key=True)
     
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     
-    clave_administrador = models.CharField(max_length=255,null=True, blank=True)
-    telefono = models.CharField(max_length=255, null=True, blank=True)
-    rfc = models.CharField(max_length=255,null=True, blank=True)
-    edad = models.IntegerField(null=True, blank=True)
-    ocupacion = models.CharField(max_length=255,null=True, blank=True)
+    clave_administrador = models.CharField(max_length=128, null=True, blank=True)
+    telefono = models.CharField(max_length=20, null=True, blank=True)
+    rfc = models.CharField(max_length=13, null=True, blank=True)
+    edad = models.IntegerField(
+        null=True, 
+        blank=True, 
+        validators=[MinValueValidator(0), MaxValueValidator(120)]
+    )
+    ocupacion = models.CharField(max_length=255, null=True, blank=True)
     
-    creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    update = models.DateTimeField(null=True, blank=True)
+    creation = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return "Clave " + self.clave_administrador
+        return f"Clave: {self.clave_administrador or 'N/A'}"
+    
+    class Meta:
+        verbose_name = 'Administrador'
+        verbose_name_plural = 'Administradores'
+        ordering = ['-creation']
