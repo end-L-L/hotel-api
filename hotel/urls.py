50658630996207d957ca233b7001ca88d18e0461
Boset <boss-x86@protonmail.com>
2024-11-19 01:17:39 -0600

    update urls.py

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from hotel.views import usuarios
+
 routers = routers.DefaultRouter()
 
-urlpatterns = []
+urlpatterns = [
+    
+    # administradores
+    path("v1/administrador", usuarios.AdministradorView.as_view(), name="administrador"),
+    path("v1/lista-administradores", usuarios.AdministradoresView.as_view(), name="lista-administradores")
+
+]
