f7c0c99f15ba515dd59186ff6901138313c470ea
Boset <boss-x86@protonmail.com>
2024-11-19 18:56:20 -0600

    feature - recepcionista views
    - created "recepcionista" views

--- hotel/views/usuarios.py
+++ hotel/views/usuarios.py
@@ -4,13 +4,16 @@ from django.shortcuts import get_object_or_404
 
 from hotel.serializers import UserSerializer
 from hotel.serializers import AdministradorSerializer
+from hotel.serializers import RecepcionistaSerializer
 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
 from hotel.models import Administrador
+from hotel.models import Recepcionista
+
+# Administradores
 
 class AdministradoresView(APIView):
     
@@ -71,5 +74,69 @@ class AdministradorView(APIView):
             admin.save()
 
             return Response({"administrador_id": admin.id }, 201)
+
+        return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Recepcionistas
+
+class RecepcionistasView(APIView):
+    
+    def get(self, request, *args, **kwargs):
+        
+        recepcionistas = Recepcionista.objects.all()
+        serializer = RecepcionistaSerializer(recepcionistas, many=True)
+        
+        return Response(serializer.data)
+    
+class RecepcionistaView(APIView):
+        
+    def get(self, request, *args, **kwargs):
+        
+        recepcionista = get_object_or_404(Recepcionista, id = request.GET.get("id"))
+        recepcionista = RecepcionistaSerializer(recepcionista, many=False).data
+
+        return Response(recepcionista, 200)
+    
+    def post(self, request, *args, **kwargs):
+        
+        user = UserSerializer(data=request.data)
+        
+        if user.is_valid():
+            role = request.data['role']
+            first_name = request.data['first_name']
+            last_name = request.data['last_name']
+            email = request.data['email']
+            password = request.data['password']
+            
+            # valida email
+            existing_user = User.objects.filter(email=email).first()
+
+            if existing_user:
+                return Response({"mensaje":"Correo "+email+", ya Existe"},400)
+
+            user = User.objects.create( username = email,
+                                        email = email,
+                                        first_name = first_name,
+                                        last_name = last_name,
+                                        is_active = 1)
+
+            user.save()
+            user.set_password(password)
+            user.save()
+
+            group, created = Group.objects.get_or_create(name=role)
+            group.user_set.add(user)
+            user.save()
+
+            # perfil recepcionista
+            recepcionista = Recepcionista.objects.create(user=user,
+                                            clave_recepcionista= request.data["clave_recepcionista"],
+                                            telefono= request.data["telefono"],
+                                            rfc= request.data["rfc"].upper(),
+                                            edad= request.data["edad"],
+                                            ocupacion= request.data["ocupacion"])
+            recepcionista.save()
+
+            return Response({"recepcionista_id": recepcionista.id }, 201)
 
         return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
