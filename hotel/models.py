1c6f931e4d823142d69b3b547cbc6b16a390c889
Boset <boss-x86@protonmail.com>
2024-11-19 18:53:35 -0600

    feature - recepcionista model
    - created"recepcionista" model

--- hotel/models.py
+++ hotel/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+# Modelo de Administrador
+
 class Administrador(models.Model):
     id = models.BigAutoField(primary_key=True)
     
@@ -26,4 +28,32 @@ class Administrador(models.Model):
     class Meta:
         verbose_name = 'Administrador'
         verbose_name_plural = 'Administradores'
+        ordering = ['-creation']
+
+# Modelo de Recepcionista
+
+class Recepcionista(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    clave_recepcionista = models.CharField(max_length=128, null=True, blank=True)
+    telefono = models.CharField(max_length=20, null=True, blank=True)
+    rfc = models.CharField(max_length=13, null=True, blank=True)
+    edad = models.IntegerField(
+        null=True, 
+        blank=True, 
+        validators=[MinValueValidator(0), MaxValueValidator(120)]
+    )
+    ocupacion = models.CharField(max_length=255, null=True, blank=True)
+    
+    creation = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Clave: {self.clave_recepcionista or 'N/A'}"
+    
+    class Meta:
+        verbose_name = 'Recepcionista'
+        verbose_name_plural = 'Recepcionistas'
         ordering = ['-creation']
