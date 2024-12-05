5ee512f1c42ec02eea568f2d755801d649f0f25b
Boset <boss-x86@protonmail.com>
2024-12-05 03:37:38 -0600

    django administration

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import *
 
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'tipo', 'precio', 'disponible')
+    list_filter = ('tipo', 'disponible')
+
+class ReservacionAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'habitacion', 'fecha_entrada', 'fecha_salida')
+    list_filter = ('fecha_entrada', 'fecha_salida')
+
 admin.site.register(Administrador)
 admin.site.register(Recepcionista)
 admin.site.register(Cliente)
-admin.site.register(Habitacion)
+admin.site.register(Habitacion, HabitacionAdmin)
 admin.site.register(Tipo_Habitacion)
-admin.site.register(Reservacion)
+admin.site.register(Reservacion, ReservacionAdmin)
 
